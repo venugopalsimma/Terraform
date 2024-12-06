a7dc994cc15daae296c468bd2aeb1607062bf37a
venugopalsimma <venugopalsimma01@gmail.com>
2024-12-06 15:36:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
-resource "local_file" "pet" {
-filename = "pets.txt"
-content = "My cat is MR.Cat"
-}
-resource "random_pet" "mypet" {
-prefix = "MR"
-separator = "."
-length = "1"
+pipeline {
+    agent any
+    stages {
+        stage('Clone') {
+            steps {
+                git branch: 'main', url: 'https://github.com/venugopalsimma/Terraform.git'
+            }
+        }
+        stage('Initialize Terraform') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+        stage('Execute Terraform Apply') {
+            steps {
+                sh 'terraform apply -auto-approve'
+            }
+        }
+    }
 }
